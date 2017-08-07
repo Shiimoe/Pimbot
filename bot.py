72a217284bc89d38ea3a636738960282c6d6e3b8
Sam <SamuelFrost@tuta.io>
2017-08-07 19:50:51 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,103 @@
+import discord
+import asyncio
+from numpy import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+
+	if message.content.startswith('erp with me pimbot'):
+		await client.send_message(message.channel, '*rubs your milky thighs*')
+	if message.content.startswith('l-lewd'):
+		await client.send_message(message.channel, "I'll show you lewd! *sticks finger up butte*")
+	if message.content.startswith('bully me pimbot'):
+		await client.send_message(message.channel, "I bet that would turn you on wouldn't it you fairy")
+	if message.content.startswith('tell me about the jews pimbot'):
+		await client.send_message(message.channel, '(((they))) are putting oestrogen in the water turning the frogs and our sons gay!')
+
+    #if message.content.startswith('!best'):
+       #myid = '<@201909896357216256>'
+       #await client.send_message(message.channel, ' : %s is the best ' % myid)
+
+	#if message.content.startswith('hello <@343915078074236929>'):
+		#myid = '<@343915078074236929>'
+		#await client.send_message(message.channel, myid + ' says hello')
+
+	if message.content.startswith('<@343915078074236929>'):
+		await client.send_message(message.channel, 'h-huh')
+	if message.content.startswith('nini~'):
+		await client.send_message(message.channel, 'goodnight qt')
+	if message.content.startswith('no u'):
+		await client.send_message(message.channel, 'no me')
+	if message.content.startswith('bye bye pimbot'):
+		await client.send_message(message.channel, 'bye bye~')
+	if message.content.startswith('~github'):
+		await client.send_message(message.channel, 'fork me or contribute to my development on github: \nhttps://github.com/Shiimoe/Pimbot')
+
+
+
+
+
+
+	if message.content.startswith('a-a' and message.author.bot == False):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('b-b'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('c-c'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('d-d'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('e-e'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('f-f'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('g-g'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('h-h'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('i-i'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('j-j'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('k-k'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('l-l'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('m-m'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('n-n'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('o-o'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('p-p'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('q-q'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('r-r'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('s-s'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('t-t'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('u-u'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('v-v'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('w-w'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('x-x'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('y-y'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+	if message.content.startswith('z-z'):
+		await client.send_message(message.channel, 'Stop stuttering you gay cunt')
+
+client.run('MzQzOTE1MDc4MDc0MjM2OTI5.DGlHtA.50snhJlQlLsEmm69zh-v8zcKs5Y')
